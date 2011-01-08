74d37513295d211a96103582e6089d01b023fb43
Theofilos Intzoglou <int.teo@gmail.com>
2011-01-08 03:54:01 +0200

    Add argument parsing via argparse and add new command line options --verbose, --log-level, --version.

--- src/pifi.py
+++ src/pifi.py
@@ -6,6 +6,7 @@
 from lib.machine import ZMachine
 from plugins.qtplugin import QtPlugin
 import sys
+import argparse
 
 __author__="oscar"
 __date__ ="$17 Ιουν 2009 2:24:30 πμ$"
@@ -13,15 +14,17 @@ __date__ ="$17 Ιουν 2009 2:24:30 πμ$"
 if __name__ == "__main__":
 
     # @type f file
-    if ( len(sys.argv) == 1 or len(sys.argv[1]) == 0 or (sys.argv[1] == "-p" and len(sys.argv) < 4) or (sys.argv[1] <> "-p" and len(sys.argv) < 2)):
-        print "Usage: ",sys.argv[0],"[-p <plugin name>] <z-code file>"
-        print "Default plugin: QtPlugin"
-        sys.exit(1);
-
-    # TODO: Handle -p option
+    # @type parser argparse.ArgumentParser
+    parser = argparse.ArgumentParser(description='PiFi - Python Interactive Fiction Interpreter')
+    parser.add_argument('-p', '--plugin', help='specify the ouput plugin to use; use QtPlugin for now')
+    parser.add_argument('-v', '--verbose', action='store_true')
+    parser.add_argument('-l', '--log-level', type=int, default=0, help='debug log level (0-2); default: %(default)s')
+    parser.add_argument('-V', '--version', action='version', version='%(prog)s 0.1')
+    parser.add_argument('zfile')
+    args = parser.parse_args()
 
     try:
-        f = open(sys.argv[1],'rb');
+        f = open(args.zfile,'rb');
         #f = open("/home/oscar/games/wcastle.z5",'rb');
     except IOError as (errno,strerror):
         print "I/O error ({0}): {1}".format(errno,strerror)
