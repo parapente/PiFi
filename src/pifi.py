c0138b055cb105ef80433d852fe6d052bd972859
Theofilos Intzoglou <int.teo@gmail.com>
2024-06-25 21:37:43 +0300

    Fix plugin detection when run from outside src. Import only the plugin in use.

--- src/pifi.py
+++ src/pifi.py
@@ -4,6 +4,7 @@
 
 from lib.machine import ZMachine
 from importlib import reload, import_module
+from pathlib import Path
 from pkgutil import iter_modules
 import sys
 import argparse
@@ -12,6 +13,31 @@ import signal
 __author__ = "Theofilos Intzoglou"
 __date__ = "$17 Ιουν 2009 2:24:30 πμ$"
 
+
+def discover_plugins():
+    current_path: Path = Path(__file__).parent.resolve()
+
+    return {
+        name.removeprefix("plugin_"): "plugins." + name
+        for _, name, _ in iter_modules(path=[current_path.__str__() + "/plugins"])
+        if name.startswith("plugin_")
+    }
+
+
+class ListPluginsAction(argparse.Action):
+    def __init__(self, option_strings, dest, nargs=None, **kwargs):
+        if nargs is not None:
+            raise ValueError("nargs not allowed")
+        super().__init__(option_strings, dest, 0, **kwargs)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        print("Found plugins:")
+        discovered_plugins = discover_plugins()
+        for plugin_name in discovered_plugins.keys():
+            print(plugin_name)
+        exit(0)
+
+
 if __name__ == "__main__":
 
     signal.signal(signal.SIGINT, signal.SIG_DFL)  # Break on control-c
@@ -19,44 +45,38 @@ if __name__ == "__main__":
     # @type f file
     # @type parser argparse.ArgumentParser
     parser = argparse.ArgumentParser(
-        description='PiFi - Python Interactive Fiction Interpreter')
-    parser.add_argument(
-        '-p',
-        '--plugin',
-        help='specify the plugin to use; use QtPlugin for now',
-        default='QtPluginV3'
+        description="PiFi - Python Interactive Fiction Interpreter"
     )
     parser.add_argument(
-        '-P',
-        '--list-plugins',
-        action='store_true',
-        help='list all available plugins'
+        "-p",
+        "--plugin",
+        help="specify the plugin to use; use QtPlugin for now",
+        default="QtPluginV3",
     )
-    parser.add_argument('-v', '--verbose', action='store_true')
-    parser.add_argument('-l', '--log-level', type=int, default=0,
-                        help='debug log level (0-2); default: %(default)s')
-    parser.add_argument('-V', '--version', action='version',
-                        version='%(prog)s 0.1')
-    parser.add_argument('zfile')
+    parser.add_argument(
+        "-P",
+        "--list-plugins",
+        action=ListPluginsAction,
+        help="list all available plugins",
+    )
+    parser.add_argument("-v", "--verbose", action="store_true")
+    parser.add_argument(
+        "-l",
+        "--log-level",
+        type=int,
+        default=0,
+        help="debug log level (0-2); default: %(default)s",
+    )
+    parser.add_argument("-V", "--version", action="version", version="%(prog)s 0.1")
+    parser.add_argument("zfile")
     args = parser.parse_args()
 
-    discovered_plugins = {
-        name.removeprefix('plugin_'): import_module('plugins.' + name)
-        for _, name, _
-        in iter_modules(path=['plugins'])
-        if name.startswith('plugin_')
-    }
-
-    if args.list_plugins:
-        for plugin_name in discovered_plugins.keys():
-            print(plugin_name)
-        exit(0)
+    discovered_plugins = discover_plugins()
 
     try:
-        f = open(args.zfile, 'rb')
+        f = open(args.zfile, "rb")
     except IOError as cannot_open_file:
-        (errno, strerror) = cannot_open_file.args
-        print("I/O error ({0}): {1}".format(errno, strerror))
+        print(f"I/O error {cannot_open_file}")
         sys.exit(2)
 
     reload(sys)
@@ -66,7 +86,7 @@ if __name__ == "__main__":
         sys.exit(3)
 
     class_name = args.plugin
-    plugin = getattr(discovered_plugins[args.plugin], class_name)()
+    plugin = getattr(import_module(discovered_plugins[args.plugin]), class_name)()
 
     plugin.prepare_gui()
     plugin.set_debug_level(args.log_level)
