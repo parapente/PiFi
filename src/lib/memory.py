f749adcb80dee3291244b722dc40730ed5cad08e
Theofilos Intzoglou <int.teo@gmail.com>
2024-07-14 19:13:37 +0300

    Cleanup class

--- src/lib/memory.py
+++ src/lib/memory.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8
 
 from array import array
-import sys
-
+from io import BufferedReader
+from lib.error import InvalidHighMemoryException
 from lib.singleton import Singleton
 
 __author__ = "Theofilos Intzoglou"
@@ -16,87 +16,58 @@ class ZMemory(metaclass=Singleton):
     high_beg = 0
     high_end = 0
 
-    def initialize(self, f: str = None, max_length: int = None) -> None:
-        self.mem = array('B')
-        if f:
+    def initialize(
+        self, story_file: BufferedReader = None, max_length: int = None
+    ) -> None:
+        self.mem = array("B")
+        if story_file:
             try:
-                self.mem.fromfile(f, max_length)
+                self.mem.fromfile(story_file, max_length)
             except EOFError:
-                print(('file size:', f.tell()))
-                need = max_length - f.tell()
-                self.mem.extend([0]*need)
+                print(("file size:", story_file.tell()))
+                need = max_length - story_file.tell()
+                self.mem.extend([0] * need)
                 # if ( need ) > 0:
                 #    for i in xrange(need):
                 #        self.mem.append(0)
-            self.static_beg = 256*self.mem[0x0e]+self.mem[0x0f]
-            self.high_beg = 256*self.mem[0x04]+self.mem[0x05]
+            self.static_beg = 256 * self.mem[0x0E] + self.mem[0x0F]
+            if story_file.tell() > 0xFFFF:
+                self.static_end = 0xFFFF
+            else:
+                self.static_end = story_file.tell()
+            self.high_beg = 256 * self.mem[0x04] + self.mem[0x05]
+            self.high_end = story_file.tell()
+            if self.high_beg < self.static_beg:
+                raise InvalidHighMemoryException(
+                    f"Invalid value for high memory start '{self.high_beg}'. Static memory begins at '{self.static_beg}'"
+                )
         # for i in xrange(len(data)):
         #    self.mem.append(ord(data[i]));
         # print 'Static:',self.static_beg,'High:',self.high_beg
-        # TODO: Find static_end
 
-    def read(self, offset: int) -> int:
-        """Function to be used with z-code to read a byte from specific offset"""
-        if offset >= 0:
-            if offset < self.static_beg:
-                part = 1
-            elif offset < self.static_end:
-                part = 2
-            else:
-                part = 3
-        else:
-            print("Memory:read:Error! Negative offset!")
-            sys.exit(1)
+    def get_memory_bit(self, address: int, bit: int) -> int:
+        """Read the value of a bit found at the address specified
+        in the first argument
 
-        if part == 1 or part == 2:
-            return self.mem[offset]
-        else:
-            print("Memory:read:Error! Trying to access high memory!")
-            sys.exit(1)
+        Args:
+            address (int): the address offset
+            bit (int): the bit number (0-8)
 
-    def write(self, offset: int, data: int) -> None:
-        """Function to be used with z-code to write to specific offset a byte"""
-        if offset >= 0:
-            if offset < self.static_beg:
-                part = 1
-            elif offset < self.static_end:
-                part = 2
-            else:
-                part = 3
-        else:
-            print("Memory:write:Error! Negative offset!")
-            sys.exit(1)
-
-        if part == 1:
-            self.mem[offset] = data
-        elif part == 2:
-            print("Memory:write:Error! Static memory is read only!")
-            sys.exit(2)
-        else:
-            print("Memory:write:Error! Trying to access high memory!")
-            sys.exit(1)
-
-    def p_read(self, offset: int) -> int:
-        """Generic function for reading bytes from Z memory"""
-        if offset < 0:
-            print("Memory:p_read:Error! Negative offset!")
-            sys.exit(1)
-        else:
-            return self.mem[offset]
-
-    def p_write(self, offset: int, data: int) -> None:
-        """Generic function for writing bytes to Z memory"""
-        if offset < 0:
-            print("Memory:p_write:Error! Negative offset!")
-            sys.exit(1)
-        else:
-            self.mem[offset] = data
-
-    def get_memory_bit(self, address: int, bit: int):
+        Returns:
+            int: 0 or 1
+        """
         mask = 0b1 << bit
-        return self.mem[address] & mask
+        return int(bool(self.mem[address] & mask))
 
     def set_memory_bit(self, address: int, bit: int, value: int):
-        self.mem[address] &= 0xff - (1 << bit)
-        if (value):
+        """Set the value of a bit found at the address specified
+        in the first argument
+
+        Args:
+            address (int): the address offset
+            bit (int): the bit number (0-8)
+            value (int): the value (0 or 1)
+        """
+        self.mem[address] &= 0xFF - (1 << bit)
+        if value:
             self.mem[address] |= 1 << bit
