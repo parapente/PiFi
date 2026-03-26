9625e3c45ebd64c0ec5f68ea0d7d6aca7d2e6ad5
Theofilos Intzoglou <int.teo@gmail.com>
2026-03-27 00:01:07 +0200

    Convert to dataclass and add types

--- src/lib/stream.py
+++ src/lib/stream.py
@@ -1,9 +1,12 @@
+from dataclasses import dataclass
+
 from lib.container.container import Container
 
 
+@dataclass
 class ZStream:
-    selected = False
-    filename = None
+    selected: bool = False
+    filename: str | None = None
 
 
 # Register the class with the container
